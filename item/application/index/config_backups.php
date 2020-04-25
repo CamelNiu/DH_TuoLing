1fc103693a9d82cbb212310375321ad4ac4eec6e
NiuShao.Lenovo <camel_niu@163.com>
2020-04-25 19:11:14 +0800

    设置全局环境变量，初始化项目

--- item/application/index/config_backups.php
+++ item/application/index/config_backups.php
@@ -1,9 +1,12 @@
 <?php
 //配置文件
-return [
-    'view_replace_str' => [
-        '__HOME_STYLE__' => 'http://127.0.0.1/DH_TuoLing/item/public/static/IndexStyle',
-        '__HOMEPAGE_IMG__' => 'http://127.0.0.1/DH_TuoLing/item/public/uploads/HomepageImg',
-        '__BLOG_IMG__'=> 'http://127.0.0.1/DH_TuoLing/item/public/uploads/BlogsImg'
-    ],
-];
+
+if( ENV == 'dev' ){
+    return [
+        'view_replace_str' => [
+            '__HOME_STYLE__' => 'http://127.0.0.1/DH_TuoLing/item/public/static/IndexStyle',
+            '__HOMEPAGE_IMG__' => 'http://127.0.0.1/DH_TuoLing/item/public/uploads/HomepageImg',
+            '__BLOG_IMG__'=> 'http://127.0.0.1/DH_TuoLing/item/public/uploads/BlogsImg'
+        ],
+    ];
+}
---
+++ item/config/database_backups.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'hostname' => '127.0.0.1',
+    'database' => '',
+    'username' => '',
+    'password' => '',
+    'hostport' => '3306',
+];
