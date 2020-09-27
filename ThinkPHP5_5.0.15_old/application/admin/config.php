8d85413830799108905f58a52f57f64823e5f6ff
NiuShaoGang <camel_niu@163.COM>
2020-09-27 17:49:01 +0800

    暴力破解

--- ThinkPHP5_5.0.15_old/application/admin/config.php
+++ ThinkPHP5_5.0.15_old/application/admin/config.php
@@ -15,10 +15,10 @@ if( ENV == 'dev' ){
     return [
 
         'view_replace_str' => [
-            '__ADMIN_STYLE__' => 'http://localhost/DH_TuoLing/ThinkPHP5_5.0.15_old/public/static/AdminStyle',
-            '__KINDEDITOR_PATH__' => 'http://localhost/DH_TuoLing/ThinkPHP5_5.0.15_old/public/static/kindeditor',
-            '__UEDITOR_PATH__' => 'http://localhost/DH_TuoLing/ThinkPHP5_5.0.15_old/public/static/ueditor1_4_3_3-utf8-php/utf8-php',
-            '__HOMEPAGE_IMG__' => 'http://localhost/DH_TuoLing/ThinkPHP5_5.0.15_old/public/uploads/HomepageImg'
+            '__ADMIN_STYLE__'     => 'http://127.0.0.1:18080/DH_TuoLing/ThinkPHP5_5.0.15_old/public/static/AdminStyle',
+            '__KINDEDITOR_PATH__' => 'http://127.0.0.1:18080/DH_TuoLing/ThinkPHP5_5.0.15_old/public/static/kindeditor',
+            '__UEDITOR_PATH__'    => 'http://127.0.0.1:18080/DH_TuoLing/ThinkPHP5_5.0.15_old/public/static/ueditor1_4_3_3-utf8-php/utf8-php',
+            '__HOMEPAGE_IMG__'    => 'http://127.0.0.1:18080/DH_TuoLing/ThinkPHP5_5.0.15_old/public/uploads/HomepageImg'
         ],
     ];
 }
